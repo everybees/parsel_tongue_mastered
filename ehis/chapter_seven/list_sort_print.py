f65cd9d90753c12f942a3fe46250e364f35643ad
Ehis Edemakhiota <edemaehiz@gmail.com>
2021-08-27 15:42:02 +0100

    added number 47 and 43

--- ehis/chapter_seven/list_sort_print.py
+++ ehis/chapter_seven/list_sort_print.py
@@ -1,8 +1,14 @@
 from typing import List
 
-list_of_chars: List[str] = [char for char in input("""
-Enter a string
-I will sort the characters in it 
-And reverse it! -> """)]
-string_result: str = "".join(sorted(list_of_chars))
-print(string_result)
+
+def list_sort_print():
+    list_of_chars: List[str] = [char for char in input("""
+    Enter a string
+    I will sort the characters in it 
+    And reverse it! -> """)]
+    string_result: str = "".join(sorted(list_of_chars))
+    print(string_result)
+
+
+if __name__ == "__main__":
+    list_sort_print()
