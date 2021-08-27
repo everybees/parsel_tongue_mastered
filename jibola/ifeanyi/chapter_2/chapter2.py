2653753cc50b8adf1b259acf2a59aecef9549475
Ifeanyi Osuji <oindubuisi@gmail.com>
2021-08-27 13:51:45 +0100

    number 10 and 25

--- jibola/ifeanyi/chapter_2/chapter2.py
+++
@@ -1,8 +0,0 @@
-import math
-
-number = int(input('Enter a number '))
-perfect_square = math.sqrt(number) ** 2
-if perfect_square == number:
-    print('Its a perfect square')
-else:
-    print('Not a perfect square')
---
+++ jibola/ifeanyi/chapter_2/number_25.py
@@ -0,0 +1,4 @@
+number = int(input('Enter two digit numbers'))
+
+if (number ** 2) % 100 == number:
+    print(number)
