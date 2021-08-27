2429ba2952891e1c853a3d1bdd589543c2ca352a
Oluwatobi Jolayemi <jolayemi.tobi@gmail.com>
2021-08-27 22:21:15 +1100

    chapter 2;Number 10

--- oluwatobi/chapter_two/question_10.py
+++ oluwatobi/chapter_two/question_10.py
@@ -3,8 +3,7 @@ import math
 sentinel = 0
 while sentinel != -1:
     number = int(input("Enter number: "))
-    root = math.sqrt(number)
-    if root ** 2 == number:
+    if math.ceil(math.sqrt(number)) == math.floor(math.sqrt(number)):
         print("It is a perfect square")
         sentinel = -1
     else:
