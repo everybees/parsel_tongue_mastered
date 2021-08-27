60d5a4d304acf5cf27c0f8f3b830538ff9ba766c
Okany Onyeka E <Okanynuel@gmail.com>
2021-08-27 15:12:22 +0100

    submission

---
+++ Emmanuel/Chapter_seven/question_26.py
@@ -0,0 +1,4 @@
+
+num = 40
+for i in range(2,num,2):
+     print (i),
---
+++ Emmanuel/Chapter_seven/question_43.py
@@ -0,0 +1,3 @@
+string1 = "Emmanuel"
+print("Type of string: ", type(string1))
+print("String coverted to list :\n", list(string1))
