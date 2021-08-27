8a21769b5e30f4c6fe848917e7585c5f108b2ddb
Janet Ishola <janetishola2010@gmail.com>
2021-08-27 15:45:38 +0100

    chapter seven updated

--- janet/chapter_seven/question_47.py
+++ janet/chapter_seven/question_47.py
@@ -1,14 +1,19 @@
 def max_number(lists):
     maximum = lists[0]
-    minimum = lists[0]
     for i in range(len(lists)):
         if lists[i] > maximum:
             maximum = lists[i]
+    return maximum
+
+
+def min_number(lists):
+    minimum = lists[0]
+    for i in range(len(lists)):
         if lists[i] < minimum:
             minimum = lists[i]
 
-    print("The maximum number is ", maximum)
-    print("The minimum number is ", minimum)
+    return minimum
 
 
-print(max_number([65,73,91,26,92,100,150]))
+print("The maximum number is ", max_number([65, 73, 91, 26, 92, 100, 150]))
+print("The minimum number is ", min_number([65, 73, 91, 26, 92, 100, 150]))
